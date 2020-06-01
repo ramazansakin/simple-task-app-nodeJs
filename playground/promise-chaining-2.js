7c7e367baeb9e8031a1dfc3aef6efc924408227a
Ramazan Sakin <ramazansakin63@gmail.com>
2020-06-01 17:23:58 +0300

    refact

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,15 +1,6 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-// Task.findByIdAndDelete('5c1a63e8f0d4c50656c5ab28').then((task) => {
-//     console.log(task)
-//     return Task.countDocuments({ completed: false })
-// }).then((result) => {
-//     console.log(result)
-// }).catch((e) => {
-//     console.log(e)
-// })
-
 const deleteTaskAndCount = async (id) => {
     const task = await Task.findByIdAndDelete(id)
     const count = await Task.countDocuments({ completed: false })
